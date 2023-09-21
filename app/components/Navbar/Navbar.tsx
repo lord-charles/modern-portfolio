39f369c4ffe6497d1dd80b3addaed54dc9a75e79
lord-charles <mwanikicharles226@gmail.com>
2023-09-21 21:29:25 +0300

    first commit

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -32,76 +32,83 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = React.useState(false);
 
     return (
-        <Disclosure as="nav" className="navbar">
-            <>
-                <div className="mx-auto max-w-7xl p-3 md:p-4 lg:px-8">
-                    <div className="relative flex h-14 sm:h-20 items-center">
-                        <div className="flex flex-1 items-center sm:justify-between">
+      <Disclosure as="nav" className="navbar">
+        <>
+          <div className="mx-auto max-w-7xl p-3 md:p-4 lg:px-8">
+            <div className="relative flex h-14 sm:h-20 items-center">
+              <div className="flex flex-1 items-center sm:justify-between p-1">
+                {/* LOGO */}
 
-                            {/* LOGO */}
-
-                            <div className="flex flex-shrink-0 items-center">
-                             
-                                <Image
-                                    className="hidden h-[130px] w-[130px] object-scale-down lg:block"
-                                      src={'/logos/logo3-removebg-preview.png'}
-                                    alt="Crypto-Logo"
-                                    width={200}
-                                    height={200}
-                                />
-                                   <Image
-                                    className="h-[100px] w-[100px] object-scale-down lg:hidden"
-                                      src={'/logos/logo3-removebg-preview.png'}
-                                    alt="Crypto-Logo"
-                                    width={200}
-                                    height={200}
-                                />
-                            </div>
-
-                            {/* LINKS */}
-
-                            <div className="hidden lg:flex items-center border-right ">
-                                <div className="flex justify-end space-x-4">
-                                    {navigation.map((item) => (
-                                        <Link
-                                            key={item.name}
-                                            href={item.href}
-                                            className={classNames(
-                                                item.current ? 'bg-gray-900' : 'navlinks text-white hover:text-offwhite hover-underline',
-                                                'px-3 py-4 rounded-md text-lg font-normal'
-                                            )}
-                                            aria-current={item.href ? 'page' : undefined}
-                                        >
-                                            {item.name}
-                                        </Link>
-                                    ))}
-                                </div>
-
-                            </div>
-                            <button className='hidden lg:flex justify-end text-xl font-semibold py-4 px-6 lg:px-12 navbutton text-white'>Contact</button>
-                            {/* <Contactusform /> */}
-                        </div>
-
-
-                        {/* DRAWER FOR MOBILE VIEW */}
-
-                        {/* DRAWER ICON */}
-
-                        <div className={`block lg:hidden z-[99] ${isOpen? 'hidden':'visible'}`}>
-                            <Bars3Icon className="block h-6 w-6 text-white" aria-hidden="true" onClick={() => setIsOpen(true)} />
-                        </div>
-
-                        {/* DRAWER LINKS DATA */}
-
-                        <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
-                            <Drawerdata />
-                        </Drawer>
-
-                    </div>
+                <div className="flex flex-shrink-0 items-center">
+                  <Image
+                    className="hidden h-[130px] w-[130px] object-scale-down lg:block"
+                    src={"/logos/logo3-removebg-preview.png"}
+                    alt="Crypto-Logo"
+                    width={200}
+                    height={200}
+                  />
+                  <Image
+                    className="h-[80px] w-[80px] object-scale-down lg:hidden"
+                    src={"/logos/logo3-removebg-preview.png"}
+                    alt="Crypto-Logo"
+                    width={200}
+                    height={200}
+                  />
                 </div>
-            </>
-        </Disclosure>
-    )
+
+                {/* LINKS */}
+
+                <div className="hidden lg:flex items-center border-right ">
+                  <div className="flex justify-end space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900"
+                            : "navlinks text-white hover:text-offwhite hover-underline",
+                          "px-3 py-4 rounded-md text-lg font-normal"
+                        )}
+                        aria-current={item.href ? "page" : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+                <button className="hidden lg:flex justify-end text-xl font-semibold py-4 px-6 lg:px-12 navbutton text-white">
+                  Contact
+                </button>
+                {/* <Contactusform /> */}
+              </div>
+
+              {/* DRAWER FOR MOBILE VIEW */}
+
+              {/* DRAWER ICON */}
+
+              <div
+                className={`block lg:hidden z-[99] ${
+                  isOpen ? "hidden" : "visible"
+                }`}
+              >
+                <Bars3Icon
+                  className="block h-6 w-6 text-white"
+                  aria-hidden="true"
+                  onClick={() => setIsOpen(true)}
+                />
+              </div>
+
+              {/* DRAWER LINKS DATA */}
+
+              <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
+                <Drawerdata />
+              </Drawer>
+            </div>
+          </div>
+        </>
+      </Disclosure>
+    );
 }
 
 export default Navbar;
