9769e22665ee08d1d12ef9494f5e4f846256ae52
lord-charles <mwanikicharles226@gmail.com>
2023-09-24 22:51:05 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,15 @@ import './globals.css';
 import '@/node_modules/react-modal-video/scss/modal-video.scss';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/index';
-import PageIllustration from './components/page-illustration';
-
+import TidioChat from "./components/Tindio";
 
 export const metadata = {
-  title: 'Charles Mwaniki',
-  description: 'portfolio',
+  title: "Charles Mwaniki",
+  description: "portfolio",
   icons: {
-    icon: '/logo.svg',
+    icon: "/logo.svg",
   },
-}
+};
 import { Inter, Architects_Daughter } from "next/font/google";
 const inter = Inter({
   subsets: ["latin"],
@@ -38,6 +37,7 @@ export default function RootLayout({
       >
         {/* <PageIllustration /> */}
         <Navbar />
+        <TidioChat />
         {children}
         <Footer />
       </body>
