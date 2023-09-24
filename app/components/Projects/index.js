2bb026884e56c461c4e0ea6702a946cda9cb06f9
lord-charles <mwanikicharles226@gmail.com>
2023-09-24 22:28:09 +0300

    first commit

--- app/components/Projects/index.js
+++ app/components/Projects/index.js
@@ -18,117 +18,123 @@ import HeroBgAnimation from "../HeroBgAnimation/index";
 const Projects = ({ openModal, setOpenModal }) => {
   const [toggle, setToggle] = useState("all");
   return (
-    <Fade
-      direction={"up"}
-      delay={400}
-      cascade
-      damping={1e-1}
-      triggerOnce={true}
-    >
-      <Container id="projects" className="overflow-x-hidden">
-        <div className="radial-bgone hidden lg:block"></div>
-        <div className="absolute top-[-100px]">
-          <HeroBgAnimation />
-        </div>
-        <div className="absolute">
-          <HeroBgAnimation />
-        </div>
-        <Wrapper>
-          <Title className=" text-bluish">Portfolio Highlights</Title>
-          {/* <Desc className=" text-bluish lg:block md:block xxxs:hidden">
+    <div id="projects">
+      <Fade
+        direction={"up"}
+        delay={400}
+        cascade
+        damping={1e-1}
+        triggerOnce={true}
+      >
+        <Container className="overflow-x-hidden">
+          <div className="radial-bgone hidden lg:block"></div>
+          <div className="absolute top-[-100px]">
+            <HeroBgAnimation />
+          </div>
+          <div className="absolute">
+            <HeroBgAnimation />
+          </div>
+          <Wrapper>
+            <Title className=" text-bluish">Portfolio Highlights</Title>
+            {/* <Desc className=" text-bluish lg:block md:block xxxs:hidden">
             Explore a selection of my diverse web and mobile app projects.
             Witness my commitment to excellence and innovation in every
             endeavor.
           </Desc> */}
-          <Desc className=" text-bluish">
-            Excellence and innovation awaits.
-          </Desc>
-          <ToggleButtonGroup className=" text-green">
-            {toggle === "all" ? (
-              <ToggleButton active value="all" onClick={() => setToggle("all")}>
-                All
-              </ToggleButton>
-            ) : (
-              <ToggleButton value="all" onClick={() => setToggle("all")}>
-                All
-              </ToggleButton>
-            )}
-            <Divider className="bg-green" />
-            {toggle === "web app" ? (
-              <ToggleButton
-                active
-                value="web app"
-                onClick={() => setToggle("web app")}
-              >
-                WEB APP&apos;S
-              </ToggleButton>
-            ) : (
-              <ToggleButton
-                value="web app"
-                onClick={() => setToggle("web app")}
-              >
-                WEB APP&apos;S
-              </ToggleButton>
-            )}
-            <Divider className="bg-green" />
-            {toggle === "android app" ? (
-              <ToggleButton
-                active
-                value="android app"
-                onClick={() => setToggle("android app")}
-              >
-                ANDROID APP&apos;S
-              </ToggleButton>
-            ) : (
-              <ToggleButton
-                value="android app"
-                onClick={() => setToggle("android app")}
-              >
-                ANDROID APP&apos;S
-              </ToggleButton>
-            )}
-            <Divider className="bg-green" />
-            {toggle === "machine learning" ? (
-              <ToggleButton
-                active
-                value="machine learning"
-                onClick={() => setToggle("machine learning")}
-              >
-                MACHINE LEARNING
-              </ToggleButton>
-            ) : (
-              <ToggleButton
-                value="machine learning"
-                onClick={() => setToggle("machine learning")}
-              >
-                MACHINE LEARNING
-              </ToggleButton>
-            )}
-          </ToggleButtonGroup>
-          <CardContainer>
-            {toggle === "all" &&
-              projects.map((project, index) => (
-                <ProjectCard
-                  key={index}
-                  project={project}
-                  openModal={openModal}
-                  setOpenModal={setOpenModal}
-                />
-              ))}
-            {projects
-              .filter((item) => item.category == toggle)
-              .map((project, index) => (
-                <ProjectCard
-                  key={index}
-                  project={project}
-                  openModal={openModal}
-                  setOpenModal={setOpenModal}
-                />
-              ))}
-          </CardContainer>
-        </Wrapper>
-      </Container>
-    </Fade>
+            <Desc className=" text-bluish">
+              Excellence and innovation awaits.
+            </Desc>
+            <ToggleButtonGroup className=" text-green">
+              {toggle === "all" ? (
+                <ToggleButton
+                  active
+                  value="all"
+                  onClick={() => setToggle("all")}
+                >
+                  All
+                </ToggleButton>
+              ) : (
+                <ToggleButton value="all" onClick={() => setToggle("all")}>
+                  All
+                </ToggleButton>
+              )}
+              <Divider className="bg-green" />
+              {toggle === "web app" ? (
+                <ToggleButton
+                  active
+                  value="web app"
+                  onClick={() => setToggle("web app")}
+                >
+                  WEB APP&apos;S
+                </ToggleButton>
+              ) : (
+                <ToggleButton
+                  value="web app"
+                  onClick={() => setToggle("web app")}
+                >
+                  WEB APP&apos;S
+                </ToggleButton>
+              )}
+              <Divider className="bg-green" />
+              {toggle === "android app" ? (
+                <ToggleButton
+                  active
+                  value="android app"
+                  onClick={() => setToggle("android app")}
+                >
+                  ANDROID APP&apos;S
+                </ToggleButton>
+              ) : (
+                <ToggleButton
+                  value="android app"
+                  onClick={() => setToggle("android app")}
+                >
+                  ANDROID APP&apos;S
+                </ToggleButton>
+              )}
+              <Divider className="bg-green" />
+              {toggle === "machine learning" ? (
+                <ToggleButton
+                  active
+                  value="machine learning"
+                  onClick={() => setToggle("machine learning")}
+                >
+                  MACHINE LEARNING
+                </ToggleButton>
+              ) : (
+                <ToggleButton
+                  value="machine learning"
+                  onClick={() => setToggle("machine learning")}
+                >
+                  MACHINE LEARNING
+                </ToggleButton>
+              )}
+            </ToggleButtonGroup>
+            <CardContainer>
+              {toggle === "all" &&
+                projects.map((project, index) => (
+                  <ProjectCard
+                    key={index}
+                    project={project}
+                    openModal={openModal}
+                    setOpenModal={setOpenModal}
+                  />
+                ))}
+              {projects
+                .filter((item) => item.category == toggle)
+                .map((project, index) => (
+                  <ProjectCard
+                    key={index}
+                    project={project}
+                    openModal={openModal}
+                    setOpenModal={setOpenModal}
+                  />
+                ))}
+            </CardContainer>
+          </Wrapper>
+        </Container>
+      </Fade>
+    </div>
   );
 };
 
